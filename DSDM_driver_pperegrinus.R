88599d097e49f70280ac57967aec5348ad88d096
Rachael McCullough <rmccullough@student.unimelb.edu.au>
2020-01-08 13:52:01 +1100

    Formatting

--- DSDM_driver_pperegrinus.R
+++ DSDM_driver_pperegrinus.R
@@ -1,5 +1,6 @@
-## DSDM for P.peregrinus
-#=========================
+# =============================================================================
+# DSDM for P.peregrinus
+# =============================================================================
 # X_train
 # beta_surv <- normal(0, 1, dim = K)
 # logit_survival_offset <- normal(0, 1)
@@ -22,8 +23,18 @@
 # logit_adult_survival <- X_train %*% beta_surv
 # adult_survival <- ilogit(logit_adult_survival)
 # juvenile_survival <- ilogit(logit_adult_survival - logit_survival_offset)
+# 
+# Section 1: Set up and preliminaries
+# Section 2: Download and clean datasets
+# Section 3: Set up model priors and functions
+# Section 4: Compile model and run mcmc
+# Section 5: Get prediction arrays
+# Section 6: Plot prediction maps
 
-
+# =============================================================================
+# Section 1: Set up and preliminaries
+# =============================================================================
+# 1.1 Install packages --------------------------------------------------------
 #Install required packages
 #install.packages("zoon")
 #install.packages("devtools")
@@ -37,17 +48,20 @@ library(dplyr)
 library(rgeos)
 library(tidyr)
 
-# Set working directory for data
+# 1.2 Set working directory for downloading data  -----------------------------
 setwd("C:/Users/racha/Google Drive (rmccullough@student.unimelb.edu.au)/MSc/Research/Computational/")
 rel_path_input <- 'pp_grids/pp_grids/'
 
-# Function for coordinates data
+# =============================================================================
+# Section 2: Download and clean datasets
+# =============================================================================
+# 2.1 Function for preparing survey site coordinate data ----------------------
 prepare_coords_data <- function (coordinates_file) {
   
   attr <- rgdal::readOGR(coordinates_file, 'Survey_points') %>%
     attributes()
   
-  coords <- cbind(attr$coords[,1], attr$coords[,2], attr$data)[,c(1:3)] 
+  coords <- cbind(attr$coords[, 1], attr$coords[, 2], attr$data)[, c(1:3)] 
   colnames(coords) <- c('Longitude', 'Latitude', 'Site_point')
   coords$Site_point <- as.character(coords$Site_point)
   
@@ -57,7 +71,7 @@ prepare_coords_data <- function (coordinates_file) {
   
 }
 
-# Function for cleaning survey data
+# 2.2 Function for cleaning survey data into required format ------------------
 clean_pp<-function(file, my_pts){
   pp<-read.csv(file)
   pp_clean <- pp %>%
@@ -65,22 +79,20 @@ clean_pp<-function(file, my_pts){
     group_by(Site, Point) %>%
     summarise(Count = sum(RT)) %>% 
     merge(my_pts, by = c('Site', 'Point'))
-  
   return(pp_clean)
 }
 
-# Load survey data
+# 2.3 Load site and survey data -----------------------------------------------
 my_pts <- prepare_coords_data(coordinates_file = 'Survey_points.kml')
 pp_clean <- clean_pp('survey_data.csv', my_pts)
 
-#load environmental covariates
+# 2.3 Load environmental covariates -------------------------------------------
 #r_res <- .0025
 #r_ext <- c(141, 150, -39, -33)
-
 r <- aggregate(shapefile('pp_grids/pp_grids/cma.shp'), dissolve = TRUE) %>%
   as("SpatialPolygonsDataFrame")
 
-#create raster stack of environmental covariates
+# 2.4 Create raster stack of environmental covariates -------------------------
 slope <- mask(raster(x = paste0(rel_path_input, 'slope.tif')), r)
 water_prox <- mask(raster(x = paste0(rel_path_input, 'water_prox.tif')), r)
 fire <- mask(raster(x = paste0(rel_path_input, 'fire.tif')), r)
@@ -92,7 +104,6 @@ twi <- mask(raster(x = paste0(rel_path_input, 'twi.tif')), r) #terrain wetness
 pop <- mask(raster(x = paste0(rel_path_input, 'pop.tif')), r) #human population
 roads_prox <- mask(raster(x = paste0(rel_path_input, 'roads_prox.tif')), r)
 
-#stack rasters
 covariates <- scale(stack(slope,
                           water_prox,
                           fire,
@@ -104,51 +115,52 @@ covariates <- scale(stack(slope,
                           pop, 
                           roads_prox))
 
-# Mask any missing values in covariate stack (eg ocean in bay)
+# 2.5 Mask any missing values in covariate stack ------------------------------
 missing <- calc(covariates, anyNA)
 mask <- covariates[[1]]*0
 mask[missing] <- NA
 covariates <- mask(covariates, mask)
 
-# reduce size for computation
+# 2.6 Reduce size of cells for computation (change to higher res later) -------
 covariates <- aggregate(covariates, 10)
 
-# Crop to Melbourne bay region
+# 2.7 Crop stack to Melbourne bay region --------------------------------------
 e <- new("Extent", xmin = 143.9, xmax = 145.5, 
          ymin = -38.8, ymax = -37.7)
 covariates_cropped <- crop(covariates, e)
 
-##Refine/edit
-#choose covariates for survival and fecundity.
+# =============================================================================
+# Section 3: Set up model and functions
+# =============================================================================
+# 3.1 choose covariates for survival and fecundity ----------------------------
 survival_covs <- c('meananntemp','roads_prox','fire','pop','twi')
 fecundity_covs <- c('meananntemp', 'annprecip', 'slope', 'water_prox', 'ndvi')
 all_covs <- c(survival_covs, fecundity_covs)
 ncov_fecundity <- length(fecundity_covs)
 ncov_survival <- length(survival_covs)
 
-# Enter survival/fecundity data from papers here
-# Placeholder numbers for now for testing
+# 3.2 Include data from published papers --------------------------------------
 default_logit_survival_adult <- qlogis(0.64)
 default_logit_survival_juvenile <- qlogis(0.33)
 default_log_fecundity <- log(1.23)
 
-# Extract covariates at survey locations
+# 3.3 Extract covariates at survey locations ----------------------------------
 vals <- raster::extract(covariates_cropped, pp_clean[, c('Longitude', 'Latitude')], cellnumbers = TRUE)
 dat <- cbind(pp_clean, vals)
 
-#remove rows with NA values for covariates, and scale
+# 3.4 remove rows with NA values and scale ------------------------------------
 dat_clean <- dat %>%
   na.omit() %>%
   #mutate_at(vars(all_covs), scale) %>%
   mutate_at(vars(all_covs), as.numeric) %>%
   rename(cell_number = cells)
 
+# 3.5 Define priors for model -------------------------------------------------
 beta_fecundity <- normal(0, 1/ncov_fecundity, dim = ncov_fecundity)
 beta_survival <- normal(0, 1/ncov_survival, dim = ncov_survival)
 likelihood_intercept <- variable()
 
-# calculate survival, given covariates
-#survival
+# 3.6 Function for calculating survival, given covariates ---------------------
 get_survival <- function(covs){
   x_survival <- covs[, survival_covs]
   survival_logit_adult <- default_logit_survival_adult + x_survival %*% beta_survival
@@ -158,14 +170,14 @@ get_survival <- function(covs){
   survival <- list(adult = adult_survival, juvenile = juvenile_survival)
 }
 
-#fecundity
+# 3.7 Function for calculating fecundity, given covariates --------------------
 get_fecundity <- function(covs){
   x_fecundity <- covs[, fecundity_covs]
   fecundity_log = default_log_fecundity + x_fecundity %*% beta_fecundity
   fecundity = exp(fecundity_log)
 }
 
-#lambda
+# 3.8 Function for calculating lambda, given covariates -----------------------
 get_lambda <- function(survival, fecundity){
   top_row <- cbind(fecundity * survival$juvenile,
                    fecundity * survival$adult)
@@ -176,67 +188,53 @@ get_lambda <- function(survival, fecundity){
   lambda <- iterated$lambda
 }
 
-#probability of presence
+# 3.9 Function for calculating probability of presence ------------------------
 get_prob <- function(lambda){
   icloglog(likelihood_intercept + log(lambda))
 }
 
-#poisson rate
+# 3.10 Function for calculating count -----------------------------------------
 get_rate <- function(lambda){
   exp(likelihood_intercept)*lambda
 }
 
-#get survival and fecundity
+# =============================================================================
+# Section 4: Compile model and run mcmc
+# =============================================================================
+# 4.1 Get survival and fecundity for survey sites -----------------------------
 survival <- get_survival(dat_clean)
 fecundity <- get_fecundity(dat_clean)
 lambdas <- get_lambda(survival, fecundity)
 prob <- get_prob(lambdas)
 rate <- get_rate(lambdas)
 
-#likelihood for PA
-# To finish
-#presence<- dat_clean %>%
-  #mutate_at('Count', as.logical) %>%
-  #mutate_at('Count', as.numeric)
-#distribution(presence)<-binomial(prob)
+# 4.2 Define likelihood function for presence/absence data --------------------
+presence <- dat_clean %>%
+  mutate_at('Count', as.numeric) %>%
+  mutate(PA = ifelse(Count == 0, 0, 1))
 
-#likelihood for counts
+presence <- presence[, 'PA']
+
+distribution(presence) <- bernoulli(prob)
+
+# 4.3 Define likelihood function for count data  ------------------------------
 abundance <- dat_clean[, 'Count']
 distribution(abundance) <- poisson(rate)
 
-#fit model
+# 4.4 Fit model ---------------------------------------------------------------
 m <- model(beta_fecundity,
            beta_survival,
            likelihood_intercept)
 
+# 4.5 Run MCMC chain ----------------------------------------------------------
 draws <- mcmc(m, n_samples = 500, chains = 2)
 # summary(draws)
 # mcmc_hist(draws)
 
-# Predicting from the model
-
-# Get covariate values for all pixels in raster
-# Using only cropped Melbourne bay region
-
-# scale prediction covariates by mean and sd of survey site covariates
-# Columns of covariates only
-# dat_means <- dat %>%
-#   na.omit() %>%
-#   select(all_covs) %>%
-#   summarise_all(mean) %>%
-#   as.numeric()
-# 
-# dat_sds <- dat %>%
-#   na.omit() %>%
-#   select(all_covs) %>%
-#   summarise_all(sd)
-
-# covariates_predict_clean <- covariates_predict %>%
-#   as.data.frame() %>%
-#   na.omit() %>%
-#   mutate_at(vars(all_covs), scale(center = colMeans(cols), scale = apply(na.omit(cols), 2, sd))) %>%
-#   mutate_at(vars(all_covs), as.numeric)
-
+# =============================================================================
+# Section 5: Get prediction arrays
+# =============================================================================
+# 5.1 Function to scale covariates
 scale_covs <- function (covs, means, sds) {
   cols_scale <- match(names(means), colnames(covs))
   covs_sub <- covs[, cols_scale]
@@ -246,22 +244,24 @@ scale_covs <- function (covs, means, sds) {
   covs
 }
 
+# 5.1 Scale bay region covariates by survey site covariates -------------------
 cols <- select(dat_clean, all_covs)
 dat_means <- colMeans(cols)
 dat_sds <- apply(cols, 2, sd)
 covariates_predict <- scale_covs(raster::extract(covariates_cropped, seq(1, ncell(covariates_cropped), 1), df = TRUE), dat_means, dat_sds) %>%
   na.omit()
 
-#prediction arrays
+# 5.2 Get prediction arrays ---------------------------------------------------
 survival_predict <- get_survival(covariates_predict)
 fecundity_predict <- get_fecundity(covariates_predict)
 lambdas_predict <- get_lambda(survival_predict, fecundity_predict)
 rate_predict <- get_rate(lambdas_predict)
+#prob_presence_predict <- 
 
-# map posterior mean estimates
-# idx is index of nonNA cells in prediction array
+# 5.3 Get cell IDs of non-NA cells in prediction region -----------------------
 idx <- which(!is.na(getValues(covariates_cropped[[1]])))
-  
+
+# 5.4 Function for mapping mean of posterior sample for map grid cells --------
 map_variable <- function (greta_array, draws) {
   vals <- calculate(greta_array, draws)
   vals_mat <- as.matrix(vals)
@@ -271,7 +271,16 @@ map_variable <- function (greta_array, draws) {
   map
 }
 
+# 5.5 Get values for mapping --------------------------------------------------
 lambda_map <- map_variable(lambdas_predict, draws)
 rate_map <- map_variable(rate_predict, draws)
 fec_map <- map_variable(fecundity_predict, draws)
 surv_map <- map_variable(survival_predict$adult, draws)
+#prob_presence_map <- 
+
+# =============================================================================
+# Section 6: Plot prediction maps
+# =============================================================================
+
+
+
